5011eb2a7e8ff3726965bc97b4e1cba9b1b1cd2a
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-29 19:44:01 -0800

    refactor for accuracy

--- teleo.py
+++ teleo.py
@@ -1,4 +1,5 @@
-import os
+from pathlib import Path
+# raw_file = Path(“pages.txt”)
 
 
 '''
@@ -9,20 +10,30 @@ route list, the longest matching prefix must be used. Choose the cheapest route 
 
 
 def read_routes(phone):
+    matches = []
     found = False
     with open("route-costs-106000.txt", "r") as f:
         for line in f:
             line = line.rstrip('\n')
-            line.split(",")
-            if line[0] in phone:
-
+            route = line.split(",")
+            # print(route[0][:4])
+            if route[0][:4] == phone[:4]:
+                # print(route[0][:4])
                 found = True
-                line = ''.join(line)
-                print(line)
-            with open("route-costs-1.txt", "w") as result:
-                result.write("{}\n".format(line))
-                result.close()
-                break
+                line = ','.join(route)
+                matches.append(line)
+        # print(matches)
+        lowest = matches[0]
+        for item in matches:
+            item.split(',')
+            if item[1] < lowest.split(',')[1]:
+                lowest = item
+
+        low_cost = ''.join(lowest)
+        print(low_cost)
+        with open("route-costs-1.txt", "w") as result:
+            result.write("{}\n".format(low_cost))
+            result.close()
     if not found:
         print("Nothing found")
 
