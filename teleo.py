93b7aedd71cc2b9bfe4c5c21309852d86fa7a215
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-29 17:56:25 -0800

    first commit

---
+++ teleo.py
@@ -0,0 +1,5 @@
+'''
+Normalized numbers examples: US +12223334444, Japan +81223334444, UK +4422233334444.
+Routes: path through a carriers phone network. When finding a route to use in a single carrier's
+route list, the longest matching prefix must be used. Choose the cheapest route when there are multiple carriers.
+'''
