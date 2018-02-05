839b5682adb3c09a24e570d064d796d07f18d4d4
Phyllis Wong <wong.phyllism@gmail.com>
2018-02-04 18:33:54 -0800

    add doc string

--- teleo.py
+++ teleo.py
@@ -8,6 +8,11 @@ Normalized numbers examples: US +12223334444, Japan +81223334444, UK +4422233334
 Routes: path through a carriers phone network. When finding a route to use in a single carrier's
 route list, the longest matching prefix must be used. Choose the cheapest route when there are multiple carriers.
 '''
+
+"""
+Try using a BinarySearchTree, because you can't find the exact key of the dict().
+"""
+
 def read_one_num():
     with open(nums, "r") as f:
         phone = f.readline()
